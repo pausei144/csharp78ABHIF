fdc9a754eeb17d0898c0b66acd7a2c35b727b432
seipaa14 <seipaa14@htlkaindorf.at>
2017-11-06 13:50:31 +0100

    Initialized project

---
+++ TaskSheet_03_Artikel_Lieferant/TaskSheet_03_Artikel_Lieferant/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSheet_03_Artikel_Lieferant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("initialized");
+        }
+    }
+}
